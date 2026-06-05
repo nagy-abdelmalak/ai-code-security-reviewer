1370b394e7fc8986dad696e67f8f0c6f0031331a
Nagy Abdelmalak <nagy.abdelmalak1@gamil.com>
2026-06-05 14:15:29 +0200

    Integrate Bandit for static analysis in /review endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import tempfile
+import subprocess
 
 class CodeRequest(BaseModel):
     code: str
@@ -12,17 +14,33 @@ def root():
 
 @app.post("/review")
 def analyze(request: CodeRequest):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as tmp:
+        tmp.write(request.code.encode())
+        tmp_path = tmp.name
 
-    if "eval(" in request.code:
+    try:
+        result = subprocess.run(
+            ["bandit", "-f", "json", tmp_path], 
+            capture_output=True, 
+            text=True
+        )
+        return result.stdout
+
+    except subprocess.CalledProcessError as e:
         return {
-            "vulnerabilities": [
-                {   "type": "Code Injection",
-                    "description": "The request contains a code injection vulnerability.",
-                    "severity": "High",
-                    "line_number": 10,
-                    "file_name": "main.py",
-                    "suggestions": "Use a safer function instead of eval.",
-                }
-            ]
+            "message": "Error running bandit",
+            "error": str(e)
         }
-    return {"message": "No vulnerabilities found."}
+    # if "eval(" in request.code:
+    #     return {
+    #         "vulnerabilities": [
+    #             {   "type": "Code Injection",
+    #                 "description": "The request contains a code injection vulnerability.",
+    #                 "severity": "High",
+    #                 "line_number": 10,
+    #                 "file_name": "main.py",
+    #                 "suggestions": "Use a safer function instead of eval.",
+    #             }
+    #         ]
+    #     }
+    # return {"message": "No vulnerabilities found."}
