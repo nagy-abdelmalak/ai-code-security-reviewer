6c0ea491b10f3a9dc65859afaba5933f7340ada3
Nagy Abdelmalak <nagy.abdelmalak1@gamil.com>
2026-06-19 09:05:19 +0200

    feat(models): update models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,3 @@
 from app.models.user import Role, User
 from app.models.auditor_assignment import AuditorAssignment
 from app.models.audit_event import AuditEvent, EventType
-
-__all__=["User", "Role"]
