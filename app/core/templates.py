9fefa7491dfa1a9e76bf153524df80febc0f18a8
Nagy Abdelmalak <nagy.abdelmalak1@gamil.com>
2026-06-22 12:53:52 +0200

    fix(templates): add .parent to refer to app/core/templates instead of app/app/core/templates

--- app/core/templates.py
+++ app/core/templates.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 
-TEMPLATE_DIR = Path(__file__).parent / "templates"
+TEMPLATE_DIR = Path(__file__).parent.parent / "templates"
 templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
